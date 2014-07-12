0f9062121e6db4693bb1a4bad370822bf4459285
Luke Bartel <bartelluke@gmail.com>
2014-07-12 11:02:18 -0600

    first text works, second one still need double quote escape

--- lib/employee_filter.rb
+++ lib/employee_filter.rb
@@ -8,19 +8,24 @@ class EmployeeFilter
   def started_before_2006
     before_2006 = []
     @employees.each do |employee|
-      before_2006 << employee if employee[:name] == "Beaulah"
-      end
+      before_2006 << employee if employee[:start_date].year < 2006
+    end
 
     before_2006
   end
 
   def all_with_start_date
     employees_with_start_dates = []
-    employee_string = ""
-    @employees.each do |employee|
-      employee_string << "#{employee[:first_name]} #{employee[:last_name]} (#{employee[:title]}) -"+ " #{employee[:start_date].strftime("%m/%d/%Y")},"
+    employee_string_1 = ""
+    employee_string_2 = ""
+    @employees.each_with_index do |employee, i|
+      employee_string_1 << %Q{#{employee[:first_name]} #{employee[:last_name]} (#{employee[:title]}) - #{employee[:start_date].strftime("%-m/%-d/%Y")}",} if i == 0
     end
-    employees_with_start_dates << employee_string
+    @employees.each_with_index do |employee, i|
+      employee_string_2 << " ""#{employee[:first_name]} #{employee[:last_name]} (#{employee[:title]}) - #{employee[:start_date].strftime("%-m/%-d/%Y")}" if i == 1
+    end
+
+    employees_with_start_dates << employee_string_1 + employee_string_2
     employees_with_start_dates
   end
 
